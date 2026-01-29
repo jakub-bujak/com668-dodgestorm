0cd5020f568855da9218ddcf56b6a393e30c7130
Jakub Bujak <Bujak-J1@ulster.ac.uk>
2026-01-29 07:02:25 +0000

    Mobile Fix 4

--- app/static/Build/static.loader.js
+++ app/static/Build/static.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55828356,
+    wasmFileSize: 55828345,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
